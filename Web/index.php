1ec896c30ff3ca566a51ca4ee66c0e5fc7519487
John-Nicholas Furst <J-N@John-Nicholas.net>
2010-10-12 02:46:03 -0400

    destroy the array between loops

--- Web/index.php
+++ Web/index.php
@@ -96,15 +96,15 @@ include ('tree.php');?>
 												foreach($subs as $key => $value)
 												{
 													$command2 = "/var/www/major/weblogin/getdescription.pl $value";
-													exec($command2  . " 2>&1", $desc$key);
+													exec($command2  . " 2>&1", $desc);
 													
 													echo "
 													<tr class='listRowAlt'>
 														<td>$value</td>
 														<td></td>
-														<td>$desc$key[0]</td>
+														<td>$desc[0]</td>
 													</tr>";
-												
+												unset($desc);
 												}?>
 	
 												
