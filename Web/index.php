c768bd5933408571fe41367e6c578f210009fb86
jay <jay@swganh.org>
2010-10-14 18:21:02 -0400

    pull the username from the xml gateway / webloin

--- Web/index.php
+++ Web/index.php
@@ -6,8 +6,79 @@ $title = "Domonator";
 $name = "Domonator";
 include ('header_refresh.php');
 
-$username = "broglek@bu.edu";
+//$username = "broglek@bu.edu";
+
+# PHP example authenticated call
+#
+# Roland Jaeckel 8/19/2004
+#
+# include the mechanisms to send and receive XML package
+require('edu/bu/nis/XmlGateway/Connector/VendorRequest.php');
+require('edu/bu/nis/XmlGateway/Connector/BuResponse.php');
+#
+#
+# The vendor needs to handle the session ID from UIS internally
+# to be able to communicate with the XML gateway
+# This example stores it in PHP session variable
+session_start();
+$session = $_SESSION['xml_gateway_session'];
+# instantiate object
+$request = new VendorRequest();
+$request->setUrl('http://www-test.bu.edu/link/bin/uiscgi_shared_xml.pl/uismpl');
+$request->setSession($session);
+$request->setParameter('ModuleName', $_GET['ModuleName']);
+$request->setSyncUrl();
+
+$response	= $request->getResponse();
+
+if ($request->isError()) {
+	$ErrorMsg = $request->getErrorMsg();
+	print "System Error: $ErrorMsg\n";
+	exit;
+}
+$_SESSION['xml_gateway_session'] = $response->getSession();
+
+if ($response->get_Type() == 'ERROR') {
+	# to see what's going on
+	$responseHTML = $response->getHtml();
+	print($responseHTML);
+} elseif ($response->get_Type() == 'LOGIN') {
+	# to see what's going on
+	$responseHTML = $response->getHtml();
+	print($responseHTML);
+} elseif ($response->get_Type() == 'DATA') {
+	$user	= $response->getAlias();
+	$UserId		= $response->getId();
+	$UserRealName	= $response->getName();
+	$UserEmail	= $response->getEmail();
+
+
+	print <<<XXX
+		<html>
+			<head>
+				<title>User Information</title>
+			</head>
+			<body>
+				<h3>USER INFORMATION</h3>
+				<hr>
+				<br>
+				<p>User Alias: <b>$UserAlias</b></p>
+				<p>User Id:&nbsp;&nbsp;&nbsp;&nbsp;<b>$UserId</b></p>
+				<p>User Name:&nbsp;&nbsp;<b>$UserName</b></p>
+				<p>User Email:&nbsp;&nbsp;<b>$UserEmail</b></p>
+			</body>
+		</html>
+XXX;
+
+} else {
+	print "XmlGateway Error: Unsupported data type $response->getType()\n";
+}
+
+$username = "$user@bu.edu";
 ?>
+
+
+
 									<table cellpadding="0" cellspacing="0" width="100%" id="tabBar">
 										<tr>
 											<td class="tabOn">
